9d6730f94aba22943bf2e1716d8dc7165add5a04
enjinerd <dev@ron.my.id>
2021-12-02 00:03:01 +0700

    fix(controllers/users): Remove password response from login and register endpoint
    - For security purpose

--- controllers/users/response/register.go
+++ controllers/users/response/register.go
@@ -9,7 +9,6 @@ type RegUserResponse struct {
 	CreatedAt time.Time `json:"created_at"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
 }
 
 func FromRegDomain(domain users.Domain) RegUserResponse {
@@ -17,6 +16,5 @@ func FromRegDomain(domain users.Domain) RegUserResponse {
 		CreatedAt: domain.CreatedAt,
 		Username:  domain.Username,
 		Email:     domain.Email,
-		Password:  domain.Password,
 	}
 }
