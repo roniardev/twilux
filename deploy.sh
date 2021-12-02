224ea71091f9f130acbf126786c1f6af6140bcd3
enjinerd <dev@ron.my.id>
2021-12-03 05:53:19 +0700

    fix(actions): fix scripts deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ if test -f "$FILE"; then
     rm -f ~/twilux/docker-compose.yml
 fi
 # download my docker-compose.yml
-wget https://gist.githubusercontent.com/enjinerd/09f552dfa935ce6198da8873c17c3271/raw/bdb1d511487ea100ac66dae19f542e8eb4164cce/docker-compose.yml
+wget https://gist.githubusercontent.com/enjinerd/09f552dfa935ce6198da8873c17c3271/raw/24b2947f48a85c7e8c6b5a91c84e3dcfb51631a9/docker-compose.yml
 # build docker containers
 docker-compose up -d
